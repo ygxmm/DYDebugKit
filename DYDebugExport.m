9dc27652942a6f1cbb9c469cc493dffa7d9cdcdc
ygxmm <94471520+ygxmm@users.noreply.github.com>
2026-08-19 17:37:02 +0800

    更新 DYDebugExport.m

--- DYDebugExport.m
+++ DYDebugExport.m
@@ -1,19 +1,111 @@
 #import "DYDebugExport.h"
+#import "DYDebugCapture.h"
 
-NSURL *DYDebugExportSnapshot(DYDebugSnapshot *snapshot, NSError **error) {
-    if (!snapshot) return nil;
-    NSString *name = [NSString stringWithFormat:@"DYDebugKit-%@", [[NSUUID UUID].UUIDString substringToIndex:8]];
-    NSString *root = [NSTemporaryDirectory() stringByAppendingPathComponent:name];
-    NSFileManager *fm = [NSFileManager defaultManager];
-    if (![fm createDirectoryAtPath:root withIntermediateDirectories:YES attributes:nil error:error]) return nil;
-    NSDictionary *meta = snapshot.metadata ?: @{};
-    NSData *json = [NSJSONSerialization dataWithJSONObject:meta options:NSJSONWritingPrettyPrinted error:error];
-    if (!json) return nil;
-    [json writeToFile:[root stringByAppendingPathComponent:@"metadata.json"] options:NSDataWritingAtomic error:error];
-    [snapshot.viewTree dataUsingEncoding:NSUTF8StringEncoding].writeToFile([root stringByAppendingPathComponent:@"view-tree.txt"], YES);
-    [snapshot.viewControllers dataUsingEncoding:NSUTF8StringEncoding].writeToFile([root stringByAppendingPathComponent:@"view-controllers.txt"], YES);
-    [snapshot.screenshotPNG writeToFile:[root stringByAppendingPathComponent:@"screenshot.png"] atomically:YES];
-    NSString *index = [root stringByAppendingPathComponent:@"README.txt"];
-    [@"DYDebugKit diagnostic snapshot\nFiles: metadata.json, view-tree.txt, view-controllers.txt, screenshot.png\n" writeToFile:index atomically:YES encoding:NSUTF8StringEncoding error:error];
-    return [NSURL fileURLWithPath:root isDirectory:YES];
+@implementation DYDebugExport
+
++ (BOOL)exportSnapshot:(DYDebugSnapshot *)snapshot
+                  toURL:(NSURL *)directoryURL
+                  error:(NSError **)error
+{
+    if (snapshot == nil || directoryURL == nil) {
+        if (error) {
+            *error = [NSError errorWithDomain:@"DYDebugKit"
+                                          code:1
+                                      userInfo:@{
+                NSLocalizedDescriptionKey: @"Snapshot or destination directory is nil."
+            }];
+        }
+        return NO;
+    }
+
+    NSString *root = directoryURL.path;
+
+    if (root.length == 0) {
+        if (error) {
+            *error = [NSError errorWithDomain:@"DYDebugKit"
+                                          code:2
+                                      userInfo:@{
+                NSLocalizedDescriptionKey: @"Destination directory is invalid."
+            }];
+        }
+        return NO;
+    }
+
+    NSFileManager *fileManager = [NSFileManager defaultManager];
+
+    BOOL isDirectory = NO;
+    BOOL exists = [fileManager fileExistsAtPath:root
+                                    isDirectory:&isDirectory];
+
+    if (!exists) {
+        NSError *directoryError = nil;
+
+        BOOL created = [fileManager createDirectoryAtPath:root
+                              withIntermediateDirectories:YES
+                                               attributes:nil
+                                                    error:&directoryError];
+
+        if (!created) {
+            if (error) {
+                *error = directoryError;
+            }
+            return NO;
+        }
+    } else if (!isDirectory) {
+        if (error) {
+            *error = [NSError errorWithDomain:@"DYDebugKit"
+                                          code:3
+                                      userInfo:@{
+                NSLocalizedDescriptionKey: @"Destination path is not a directory."
+            }];
+        }
+        return NO;
+    }
+
+    NSString *viewTreePath =
+        [root stringByAppendingPathComponent:@"view-tree.txt"];
+
+    NSString *viewControllersPath =
+        [root stringByAppendingPathComponent:@"view-controllers.txt"];
+
+    NSData *viewTreeData =
+        [snapshot.viewTree dataUsingEncoding:NSUTF8StringEncoding];
+
+    NSData *viewControllersData =
+        [snapshot.viewControllers dataUsingEncoding:NSUTF8StringEncoding];
+
+    BOOL treeOK =
+        [viewTreeData writeToFile:viewTreePath atomically:YES];
+
+    if (!treeOK) {
+        if (error) {
+            *error = [NSError errorWithDomain:@"DYDebugKit"
+                                          code:4
+                                      userInfo:@{
+                NSLocalizedDescriptionKey:
+                    @"Failed to write view-tree.txt."
+            }];
+        }
+        return NO;
+    }
+
+    BOOL controllersOK =
+        [viewControllersData writeToFile:viewControllersPath
+                               atomically:YES];
+
+    if (!controllersOK) {
+        if (error) {
+            *error = [NSError errorWithDomain:@"DYDebugKit"
+                                          code:5
+                                      userInfo:@{
+                NSLocalizedDescriptionKey:
+                    @"Failed to write view-controllers.txt."
+            }];
+        }
+        return NO;
+    }
+
+    return YES;
 }
+
+@end
